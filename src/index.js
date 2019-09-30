bc605147214ea13cc86a97c212113cd52766ab0f
jiangxunliang <jxljxufe@163.com>
2019-09-30 15:19:24 +0800

    1

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 // import './ts/typescript-test.ts';
 // import * as obj from './js/es6.js';
 // console.log('es6',obj.name);
-
+import 'babel-polyfill';
 import CircleDraw, {
     test as test1,
     obj,
@@ -11,6 +11,36 @@ import CircleDraw, {
 } from './js/circleDraw.js';
 import TableLine from './js/tableLine';
 import lazyImg from './js/lazyLoadImg';
+import Test from './js/Generator/test';
+
+import co from './js/Generator/co';
+Test('I');
+
+
+function read(str) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            try {
+                let i = 100;
+                resolve(`${str}-----${i}`);
+            } catch (error) {
+                reject('获取失败!');
+            }
+        }, 1000);
+    })
+}
+
+function* gen() {
+    let r1 = yield read('1.txt');
+    console.log('r1', r1);
+    let r2 = yield read('2.txt---' + r1);
+    console.log('r2', r2);
+    return 100;
+}
+//自动执行异步遍历器
+ co(gen).then(data => {
+     console.log(1,data);
+ })
 
 console.log(new CircleDraw().showMsg('jssss'));
 console.log('tttt', test1 + '-' + obj.a + '-' + name );
---
+++ src/js/Generator/test.js
@@ -0,0 +1,10 @@
+function Test(str) {
+    say(str);
+}
+let a = ' love you tree thousands';
+
+function say(str) {
+    console.log(str + a);
+}
+
+export default Test;
